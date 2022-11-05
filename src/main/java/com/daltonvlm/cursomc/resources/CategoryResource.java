904c7dea925fa6f3d6a6fca175edf47d0be1b10f
Dalton Martins <daltonvlm@gmail.com>
2022-11-04 22:17:07 -0300

    Test first class of domain

--- src/main/java/com/daltonvlm/cursomc/resources/CategoryResource.java
+++ src/main/java/com/daltonvlm/cursomc/resources/CategoryResource.java
@@ -1,15 +1,26 @@
 package com.daltonvlm.cursomc.resources;
 
+import com.daltonvlm.cursomc.domain.Category;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String list() {
-        return "REST is working!";
+    public List<Category> list() {
+        Category c1 = new Category(1, "Informatics");
+        Category c2 = new Category(2, "Office");
+
+        List<Category> lst = new ArrayList<>();
+        lst.addAll(Arrays.asList(c1, c2));
+
+        return lst;
     }
 }
