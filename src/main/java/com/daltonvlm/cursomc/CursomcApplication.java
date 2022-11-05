44855dc1970fc7223e9fd4ad1821f58a05bbac78
Dalton Martins <daltonvlm@gmail.com>
2022-11-05 13:33:51 -0300

    Create instantiation operation

--- src/main/java/com/daltonvlm/cursomc/CursomcApplication.java
+++ src/main/java/com/daltonvlm/cursomc/CursomcApplication.java
@@ -1,13 +1,28 @@
 package com.daltonvlm.cursomc;
 
+import com.daltonvlm.cursomc.domain.Category;
+import com.daltonvlm.cursomc.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Category cat1 = new Category(null, "Informatics");
+        Category cat2 = new Category(null, "Office");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
