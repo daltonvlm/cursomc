20ea246e106c6fd1598526559938b55c13fb22c6
Dalton Martins <daltonvlm@gmail.com>
2022-11-10 18:47:52 -0300

    Personalize validation return

--- src/main/java/com/daltonvlm/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/daltonvlm/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import com.daltonvlm.cursomc.services.exceptions.DataIntegrityException;
 import com.daltonvlm.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -22,5 +24,14 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
         StandardError standardError = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(standardError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+        ValidationError validationError = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Validation error", System.currentTimeMillis());
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            validationError.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(validationError);
     }
 }
---
+++ src/main/java/com/daltonvlm/cursomc/resources/exceptions/ValidationError.java
@@ -0,0 +1,20 @@
+package com.daltonvlm.cursomc.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public ValidationError(Integer status, String msg, Long timeStamp) {
+        super(status, msg, timeStamp);
+    }
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String name, String message) {
+        errors.add(new FieldMessage(name, message));
+    }
+}
