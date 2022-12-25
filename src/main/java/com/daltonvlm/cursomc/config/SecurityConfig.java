6426f93314134f47472199ddf8a051c14cc2d6c3
Dalton Martins <daltonvlm@gmail.com>
2022-12-25 16:44:21 -0300

    Implement authorization

--- src/main/java/com/daltonvlm/cursomc/config/SecurityConfig.java
+++ src/main/java/com/daltonvlm/cursomc/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.daltonvlm.cursomc.config;
 
 import com.daltonvlm.cursomc.security.JWTAuthenticationFilter;
+import com.daltonvlm.cursomc.security.JWTAuthorizationFilter;
 import com.daltonvlm.cursomc.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,9 +15,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import java.util.Arrays;
 import java.util.List;
@@ -55,6 +58,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        http.addFilterBefore(filter, CsrfFilter.class);
     }
 
     @Override
--- src/main/java/com/daltonvlm/cursomc/security/JWTUtil.java
+++ src/main/java/com/daltonvlm/cursomc/security/JWTUtil.java
@@ -1,5 +1,6 @@
 package com.daltonvlm.cursomc.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,5 +22,29 @@ public class JWTUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(SignatureAlgorithm.HS512, secret.getBytes())
                 .compact();
+    }
+
+    public boolean isTokenValid(String token) {
+        Claims claims = getClaims(token);
+        if (claims == null) {
+            return false;
+        }
+        String username = claims.getSubject();
+        Date expirationDate = claims.getExpiration();
+        Date now = new Date(System.currentTimeMillis());
+        return username != null && expirationDate != null && now.before(expirationDate);
+    }
+
+    public String getUsername(String token) {
+        Claims claims = getClaims(token);
+        return claims != null ? claims.getSubject() : null;
+    }
+
+    private Claims getClaims(String token) {
+        try {
+            return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
