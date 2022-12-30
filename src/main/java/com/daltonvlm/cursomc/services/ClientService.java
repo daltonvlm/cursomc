df725af41a899fdfcb433fbd1c2fe1f5a7f3a0c0
Dalton Martins <daltonvlm@gmail.com>
2022-12-30 20:53:29 -0300

    Adjust image size

--- src/main/java/com/daltonvlm/cursomc/services/ClientService.java
+++ src/main/java/com/daltonvlm/cursomc/services/ClientService.java
@@ -49,6 +49,9 @@ public class ClientService {
     @Value("${img.prefix.client.profile}")
     private String prefix;
 
+    @Value("${img.profile.size}")
+    private Integer size;
+
     public Client find(Integer id) {
         UserSS user = UserService.authenticated();
         if (user == null || !user.hasRole(Profile.ADMIN) && !id.equals(user.getId())) {
@@ -123,6 +126,8 @@ public class ClientService {
         }
 
         BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+        jpgImage = imageService.cropSquare(jpgImage);
+        jpgImage = imageService.resize(jpgImage, size);
         String fileName = prefix + user.getId() + ".jpg";
         return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
     }
--- src/main/java/com/daltonvlm/cursomc/services/ImageService.java
+++ src/main/java/com/daltonvlm/cursomc/services/ImageService.java
@@ -2,6 +2,7 @@ package com.daltonvlm.cursomc.services;
 
 import com.daltonvlm.cursomc.services.exceptions.FileException;
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -46,5 +47,20 @@ public class ImageService {
         } catch (IOException e) {
             throw new FileException("Error reading file");
         }
+    }
+
+    public BufferedImage cropSquare(BufferedImage sourceImg) {
+        int min = sourceImg.getHeight() <= sourceImg.getWidth() ? sourceImg.getHeight() : sourceImg.getWidth();
+        return Scalr.crop(
+                sourceImg,
+                (sourceImg.getWidth() / 2) - (min / 2),
+                (sourceImg.getHeight() / 2) - (min / 2),
+                min,
+                min
+        );
+    }
+
+    public BufferedImage resize(BufferedImage sourceImg, int size) {
+        return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
     }
 }
